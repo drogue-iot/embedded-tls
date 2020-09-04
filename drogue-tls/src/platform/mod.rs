5005aa050118e55f47b219ebfcbc0616aa147d28
Bob McWhirter <bmcwhirt@redhat.com>
2020-09-04 14:18:38 -0400

    Move some safe things out of the unsafe block.

--- drogue-tls/src/platform/mod.rs
+++ drogue-tls/src/platform/mod.rs
@@ -1,11 +1,11 @@
 //dd#[inline]
 
 mod alloc;
-mod cortex_alloc;
+mod cortex_m_alloc;
 
 use drogue_tls_sys::types::{c_char, c_void};
 use drogue_tls_sys::{platform_set_calloc_free};
-use crate::platform::cortex_alloc::CortexMHeap;
+use crate::platform::cortex_m_alloc::CortexMHeap;
 use crate::platform::alloc::layout::Layout;
 
 #[no_mangle]
@@ -20,9 +20,6 @@ pub extern "C" fn strlen(p: *const c_char) -> usize {
     n
 }
 
-//use self::alloc::CortexMHeap;
-
-//static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 static mut ALLOCATOR: Option<CortexMHeap> = Option::None;
 
 pub fn setup_platform(start: usize, size: usize) {
@@ -36,28 +33,19 @@ pub fn setup_platform(start: usize, size: usize) {
 
 
 extern "C" fn platform_calloc_f(count: usize, size: usize) -> *mut c_void {
+    let requested_size = count * size;
+    let header_size = 2 * 4;
+    let total_size = header_size + requested_size;
+    let layout = Layout::from_size_align(total_size, 4).unwrap().pad_to_align();
+
     unsafe {
         if let Some(ref alloc) = ALLOCATOR {
-            let requested_size = count * size;
-            let header_size = 2 * 4;
-            let total_size = header_size + requested_size;
-            let layout = Layout::from_size_align(total_size, 4).unwrap().pad_to_align();
-            log::info!("calloc {} ({}+({}*{})) from free: {} used: {} -- {}",
-                total_size,
-                header_size,
-                count,
-                size,
-                alloc.free(),
-                alloc.used(),
-                layout.size());
-
             let mut ptr = alloc.alloc(layout) as *mut usize;
             *ptr = layout.size();
             ptr = ptr.offset(1);
             *ptr = layout.align();
             ptr = ptr.offset(1);
             ptr as *mut c_void
-            //0 as *mut c_void
         } else {
             0 as *mut c_void
         }
@@ -75,5 +63,4 @@ extern "C" fn platform_free_f(ptr: *mut c_void) {
             alloc.dealloc( ptr as *mut u8, Layout::from_size_align(size, align).unwrap());
         }
     }
-    log::info!("free {}", ptr as u32);
 }
