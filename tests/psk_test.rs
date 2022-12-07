e4e16985e1d19cc48c3e55b8c0a621707cc25604
Ulf Lilleengen <lulf@redhat.com>
2022-12-07 15:54:16 +0100

    debug: use tokio exec for thread

--- tests/psk_test.rs
+++ tests/psk_test.rs
@@ -13,44 +13,48 @@ use tokio::time::Duration;
 
 static INIT: Once = Once::new();
 
-fn setup() {
+fn setup() -> tokio::task::JoinHandle<()> {
     INIT.call_once(|| {
         env_logger::init();
-
-        std::thread::spawn(move || {
-            let mut child = Command::new("openssl")
-                .arg("s_server")
-                .arg("-tls1_3")
-                .arg("-psk_identity")
-                .arg("vader")
-                .arg("-psk")
-                .arg("aabbccdd")
-                .arg("-key")
-                .arg("tests/data/server-key.pem")
-                .arg("-cert")
-                .arg("tests/data/server-cert.pem")
-                .arg("-ciphersuites")
-                .arg("TLS_AES_128_GCM_SHA256")
-                .arg("-naccept")
-                .arg("1")
-                .spawn()
-                .expect("failed to execute process");
-            child.wait().unwrap();
-        });
     });
+
+    let h = tokio::task::spawn_blocking(move || {
+        let mut child = Command::new("openssl")
+            .arg("s_server")
+            .arg("-tls1_3")
+            .arg("-psk_identity")
+            .arg("vader")
+            .arg("-psk")
+            .arg("aabbccdd")
+            .arg("-key")
+            .arg("tests/data/server-key.pem")
+            .arg("-cert")
+            .arg("tests/data/server-cert.pem")
+            .arg("-ciphersuites")
+            .arg("TLS_AES_128_GCM_SHA256")
+            .arg("-naccept")
+            .arg("1")
+            .spawn()
+            .expect("failed to execute process");
+        println!("Process completed: {:?}", child.wait().unwrap());
+    });
+    println!("Returning join handle");
+    h
 }
 
-#[tokio::test]
+#[tokio::test(flavor = "multi_thread")]
 async fn test_psk_open() {
     timeout(Duration::from_secs(120), async move {
-        setup();
+        let _h = setup();
+        println!("Setup complete");
         tokio::time::sleep(core::time::Duration::from_secs(10)).await;
 
+        println!("Connectiong...");
         let stream = TcpStream::connect("127.0.0.1:4433")
             .await
             .expect("error connecting to server");
 
-        log::info!("Connected");
+        println!("Connected");
         let mut record_buffer = [0; 16384];
         let config = TlsConfig::new()
             .with_psk(&[0xaa, 0xbb, 0xcc, 0xdd], &[b"vader"])
@@ -64,6 +68,7 @@ async fn test_psk_open() {
             .open::<OsRng, NoVerify>(TlsContext::new(&config, &mut rng))
             .await
             .is_ok());
+        println!("TLS session opened");
     })
     .await
     .unwrap();
