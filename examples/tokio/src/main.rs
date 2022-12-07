9b66c719a1e0ba3f9d720c11db5fef74332a06b2
Ulf Lilleengen <lulf@redhat.com>
2022-12-07 19:17:59 +0100

    fix: tokio example

--- examples/tokio/src/main.rs
+++ examples/tokio/src/main.rs
@@ -14,14 +14,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     log::info!("Connected");
     let mut record_buffer = [0; 16384];
-    let config = TlsConfig::new()
-        .with_server_name("localhost")
-        .verify_cert(false);
+    let config = TlsConfig::new().with_server_name("localhost");
     let mut rng = OsRng;
     let mut tls: TlsConnection<FromTokio<TcpStream>, Aes128GcmSha256> =
         TlsConnection::new(FromTokio::new(stream), &mut record_buffer);
 
-    tls.open::<OsRng, std::time::SystemTime, 4096>(TlsContext::new(&config, &mut rng))
+    tls.open::<OsRng, NoVerify>(TlsContext::new(&config, &mut rng))
         .await
         .expect("error establishing TLS connection");
 
