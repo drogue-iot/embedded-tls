fdcc3d02468e08e1dc1bc1b092b2f60df8e83c71
Dániel Buga <bugadani@gmail.com>
2023-04-06 09:13:56 +0200

    Fix typo

--- src/blocking.rs
+++ src/blocking.rs
@@ -177,7 +177,7 @@ where
 
     /// Read and decrypt data filling the provided slice. The slice must be able to
     /// keep the expected amount of data that can be received in one record to avoid
-    /// loosing data.
+    /// losing data.
     pub fn read(&mut self, buf: &mut [u8]) -> Result<usize, TlsError> {
         if self.opened {
             let mut remaining = buf.len();
