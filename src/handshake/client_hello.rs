4cd297f40ce68db60868260afe19a25ff308868e
Dániel Buga <bugadani@gmail.com>
2023-04-15 04:42:58 +0200

    Clean up client_hello encoding

--- src/handshake/client_hello.rs
+++ src/handshake/client_hello.rs
@@ -9,8 +9,7 @@ use crate::config::{TlsCipherSuite, TlsConfig};
 use crate::extensions::{ClientExtension, PskKeyExchangeMode};
 use crate::handshake::{Random, LEGACY_VERSION};
 use crate::named_groups::NamedGroup;
-use crate::signature_schemes::SignatureScheme;
-use crate::supported_versions::{ProtocolVersion, TLS13};
+use crate::supported_versions::TLS13;
 use crate::TlsError;
 
 pub struct ClientHello<'config, CipherSuite>
@@ -71,57 +70,45 @@ where
         buf.push(0).map_err(|_| TlsError::EncodeError)?;
         buf.push(0).map_err(|_| TlsError::EncodeError)?;
 
-        let mut versions = Vec::<ProtocolVersion, 16>::new();
-        versions.push(TLS13).map_err(|_| TlsError::EncodeError)?;
-
-        (ClientExtension::SupportedVersions { versions }).encode(buf)?;
-
-        let mut supported_signature_algorithms = Vec::<SignatureScheme, 16>::new();
-        for scheme in self.config.signature_schemes.iter() {
-            supported_signature_algorithms
-                .push(*scheme)
-                .map_err(|_| TlsError::EncodeError)?;
+        ClientExtension::SupportedVersions {
+            versions: Vec::from_slice(&[TLS13]).unwrap(),
         }
-
-        (ClientExtension::SignatureAlgorithms {
-            supported_signature_algorithms,
-        })
         .encode(buf)?;
 
-        let mut supported_groups = Vec::<NamedGroup, 16>::new();
-        for named_group in self.config.named_groups.iter() {
-            supported_groups
-                .push(*named_group)
-                .map_err(|_| TlsError::EncodeError)?;
+        ClientExtension::SignatureAlgorithms {
+            supported_signature_algorithms: self.config.signature_schemes.clone(),
         }
+        .encode(buf)?;
 
-        (ClientExtension::SupportedGroups { supported_groups }).encode(buf)?;
+        ClientExtension::SupportedGroups {
+            supported_groups: self.config.named_groups.clone(),
+        }
+        .encode(buf)?;
 
-        (ClientExtension::PskKeyExchangeModes {
+        ClientExtension::PskKeyExchangeModes {
             modes: Vec::from_slice(&[PskKeyExchangeMode::PskDheKe]).unwrap(),
-        })
+        }
         .encode(buf)?;
 
-        (ClientExtension::KeyShare {
+        ClientExtension::KeyShare {
             group: NamedGroup::Secp256r1,
             opaque: public_key,
-        })
+        }
         .encode(buf)?;
 
         if let Some(server_name) = self.config.server_name.as_ref() {
             // TODO Add SNI extension
-            (ClientExtension::ServerName { server_name }).encode(buf)?;
+            ClientExtension::ServerName { server_name }.encode(buf)?;
         }
 
         // IMPORTANT: The pre shared keys must be encoded last, since we encode the binders
         // at a later stage
         if let Some((_, identities)) = &self.config.psk {
-            (ClientExtension::PreSharedKey {
+            ClientExtension::PreSharedKey {
                 identities: identities.clone(),
                 hash_size: <CipherSuite::Hash as OutputSizeUser>::output_size(),
-            })
-            .encode(buf)
-            .map_err(|_| TlsError::EncodeError)?;
+            }
+            .encode(buf)?;
         }
 
         //extensions.push(ClientExtension::MaxFragmentLength(
@@ -131,7 +118,7 @@ where
         // ----------------------------------------
         // ----------------------------------------
 
-        let extensions_length = (buf.len() as u16 - extension_length_marker as u16) - 2;
+        let extensions_length = (buf.len() - extension_length_marker - 2) as u16;
         //info!("extensions length: {:x?}", extensions_length.to_be_bytes());
         buf.set(extension_length_marker, extensions_length.to_be_bytes()[0])
             .map_err(|_| TlsError::EncodeError)?;
