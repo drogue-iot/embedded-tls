e2ed2ccead5699cd064573795cf7c8dbeef24ee2
Dániel Buga <bugadani@gmail.com>
2023-04-22 10:55:52 +0200

    Log the encoded extension's type

--- src/extensions/mod.rs
+++ src/extensions/mod.rs
@@ -123,61 +123,49 @@ impl ClientExtension<'_> {
     }
 
     pub(crate) fn encode(&self, buf: &mut CryptoBuffer) -> Result<(), TlsError> {
+        trace!("Encoding client extension {:?}", self.extension_type());
         buf.push_u16(self.extension_type() as u16)
             .map_err(|_| TlsError::EncodeError)?;
 
         buf.with_u16_length(|buf| {
             match self {
-                ClientExtension::ServerName { server_name } => {
-                    //info!("server name ext");
-                    buf.with_u16_length(|buf| {
-                        const NAME_TYPE_HOST: u8 = 0;
-                        buf.push(NAME_TYPE_HOST)
-                            .map_err(|_| TlsError::EncodeError)?;
+                ClientExtension::ServerName { server_name } => buf.with_u16_length(|buf| {
+                    const NAME_TYPE_HOST: u8 = 0;
+                    buf.push(NAME_TYPE_HOST)
+                        .map_err(|_| TlsError::EncodeError)?;
 
-                        buf.with_u16_length(|buf| buf.extend_from_slice(server_name.as_bytes()))
-                            .map_err(|_| TlsError::EncodeError)
-                    })
-                }
+                    buf.with_u16_length(|buf| buf.extend_from_slice(server_name.as_bytes()))
+                        .map_err(|_| TlsError::EncodeError)
+                }),
                 ClientExtension::PskKeyExchangeModes { modes } => buf.with_u8_length(|buf| {
                     for mode in modes {
                         buf.push(*mode as u8).map_err(|_| TlsError::EncodeError)?;
                     }
                     Ok(())
                 }),
-                ClientExtension::SupportedVersions { versions } => {
-                    //info!("supported versions ext");
-                    buf.with_u8_length(|buf| {
-                        for &v in versions {
-                            buf.push_u16(v).map_err(|_| TlsError::EncodeError)?;
-                        }
-                        Ok(())
-                    })
-                }
+                ClientExtension::SupportedVersions { versions } => buf.with_u8_length(|buf| {
+                    for &v in versions {
+                        buf.push_u16(v).map_err(|_| TlsError::EncodeError)?;
+                    }
+                    Ok(())
+                }),
                 ClientExtension::SignatureAlgorithms {
                     supported_signature_algorithms,
-                } => {
-                    //info!("supported sig algo ext");
-                    buf.with_u16_length(|buf| {
-                        for &a in supported_signature_algorithms {
-                            buf.push_u16(a as u16).map_err(|_| TlsError::EncodeError)?;
-                        }
-                        Ok(())
-                    })
-                }
+                } => buf.with_u16_length(|buf| {
+                    for &a in supported_signature_algorithms {
+                        buf.push_u16(a as u16).map_err(|_| TlsError::EncodeError)?;
+                    }
+                    Ok(())
+                }),
                 ClientExtension::SignatureAlgorithmsCert {
                     supported_signature_algorithms,
-                } => {
-                    //info!("supported sig algo cert ext");
-                    buf.with_u16_length(|buf| {
-                        for &a in supported_signature_algorithms {
-                            buf.push_u16(a as u16).map_err(|_| TlsError::EncodeError)?;
-                        }
-                        Ok(())
-                    })
-                }
+                } => buf.with_u16_length(|buf| {
+                    for &a in supported_signature_algorithms {
+                        buf.push_u16(a as u16).map_err(|_| TlsError::EncodeError)?;
+                    }
+                    Ok(())
+                }),
                 ClientExtension::SupportedGroups { supported_groups } => {
-                    //info!("supported groups ext");
                     buf.with_u16_length(|buf| {
                         for &g in supported_groups {
                             buf.push_u16(g as u16).map_err(|_| TlsError::EncodeError)?;
@@ -186,7 +174,6 @@ impl ClientExtension<'_> {
                     })
                 }
                 ClientExtension::KeyShare { group, opaque } => {
-                    //info!("key_share ext");
                     buf.with_u16_length(|buf| {
                         // one key-share
                         buf.push_u16(*group as u16)
@@ -224,7 +211,6 @@ impl ClientExtension<'_> {
                     Ok(())
                 }
                 ClientExtension::MaxFragmentLength(len) => {
-                    //info!("max fragment length");
                     buf.push(*len as u8).map_err(|_| TlsError::EncodeError)
                 }
             }
