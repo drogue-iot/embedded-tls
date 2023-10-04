78bc3695959b534626dac971be0ae65ca74e705d
Ulf Lilleengen <lulf@redhat.com>
2023-10-04 21:31:14 +0200

    update toolchain

--- tests/psk_test.rs
+++ tests/psk_test.rs
@@ -1,7 +1,6 @@
 #![macro_use]
 #![allow(incomplete_features)]
 #![feature(async_fn_in_trait)]
-#![feature(impl_trait_projections)]
 use embedded_io_adapters::tokio_1::FromTokio;
 use embedded_tls::*;
 use openssl::ssl;
