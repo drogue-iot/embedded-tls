3e5237b9ce3d8dee1f208dfde9f1d37349ca4f28
Marc Delling <delling@silpion.de>
2025-12-04 17:14:54 +0100

    Added test for rustpki mutual certificate auth

--- tests/rustpki_test.rs
+++ tests/rustpki_test.rs
@@ -2,8 +2,11 @@
 
 use embedded_io_adapters::tokio_1::FromTokio;
 use embedded_tls::pki::CertVerifier;
-use embedded_tls::{Aes128GcmSha256, CryptoProvider, TlsVerifier};
+use embedded_tls::{Aes128GcmSha256, CryptoProvider, SignatureScheme, TlsError, TlsVerifier};
+use p256::SecretKey;
+use p256::ecdsa::{DerSignature, SigningKey};
 use rand_core::OsRng;
+use signature::SignerMut;
 use std::net::SocketAddr;
 use std::sync::OnceLock;
 use std::time::SystemTime;
@@ -20,16 +23,27 @@ struct RustPkiProvider {
 
 impl CryptoProvider for RustPkiProvider {
     type CipherSuite = Aes128GcmSha256;
-    type Signature = &'static [u8];
+    type Signature = DerSignature;
 
     fn rng(&mut self) -> impl embedded_tls::CryptoRngCore {
         &mut self.rng
     }
 
-    fn verifier(
-        &mut self,
-    ) -> Result<&mut impl TlsVerifier<Aes128GcmSha256>, embedded_tls::TlsError> {
+    fn verifier(&mut self) -> Result<&mut impl TlsVerifier<Aes128GcmSha256>, TlsError> {
         Ok(&mut self.verifier)
+    }
+
+    fn signer(
+        &mut self,
+        key_der: &[u8],
+    ) -> Result<(impl SignerMut<Self::Signature>, SignatureScheme), TlsError> {
+        let secret_key =
+            SecretKey::from_sec1_der(key_der).map_err(|_| TlsError::InvalidPrivateKey)?;
+
+        Ok((
+            SigningKey::from(&secret_key),
+            SignatureScheme::EcdsaSecp256r1Sha256,
+        ))
     }
 }
 
@@ -78,6 +92,55 @@ async fn test_server_certificate_validation() {
     let config = TlsConfig::new()
         .with_ca(Certificate::X509(&der[..]))
         .with_server_name("localhost");
+
+    let mut tls = TlsConnection::new(
+        FromTokio::new(stream),
+        &mut read_record_buffer,
+        &mut write_record_buffer,
+    );
+
+    let open_fut = tls.open(TlsContext::new(
+        &config,
+        RustPkiProvider {
+            rng: OsRng,
+            verifier: CertVerifier::new(),
+        },
+    ));
+
+    open_fut.await.expect("error establishing TLS connection");
+
+    tls.close()
+        .await
+        .map_err(|(_, e)| e)
+        .expect("error closing session");
+}
+
+#[tokio::test]
+async fn test_mutual_certificate_validation() {
+    use embedded_tls::*;
+
+    let addr = setup().await;
+    let ca_pem = include_str!("data/ca-cert.pem");
+    let ca_der = pem_parser::pem_to_der(ca_pem);
+
+    let cli_pem = include_str!("data/client-cert.pem");
+    let cli_der = pem_parser::pem_to_der(cli_pem);
+
+    let key_pem = include_str!("data/client-key.pem");
+    let key_der = pem_parser::pem_to_der(key_pem);
+
+    let stream = tokio::net::TcpStream::connect(addr)
+        .await
+        .expect("error connecting to server");
+
+    let mut read_record_buffer = [0; 16384];
+    let mut write_record_buffer = [0; 16384];
+
+    let config = TlsConfig::new()
+        .with_ca(Certificate::X509(&ca_der[..]))
+        .with_server_name("localhost")
+        .with_cert(Certificate::X509(&cli_der[..]))
+        .with_priv_key(&key_der);
 
     let mut tls = TlsConnection::new(
         FromTokio::new(stream),
